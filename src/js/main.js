9079099520bd3541a6d6694147df4f48058a70eb
Samuel Rüegger <samuel@rueegger.me>
2024-05-18 14:11:16 +0200

    create default layout

--- src/js/main.js
+++ src/js/main.js
@@ -2,4 +2,15 @@
 import '../scss/styles.scss';
 
 /* Import JS */
-import * as bootstrap from 'bootstrap';
+//import * as bootstrap from 'bootstrap';
+
+/* Set Theme Color Mode */
+const setTheme = () => {
+  document.documentElement.setAttribute('data-bs-theme', (window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light'));
+};
+
+/* Set Color Scheme, on Page Loading */
+setTheme();
+
+/* Detect if user chnages between Dark and Light Mode */
+window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', e => setTheme());
