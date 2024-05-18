eec7538d1c7633793872ccfab7225bbf56fed2b2
Samuel Rüegger <samuel@rueegger.me>
2024-05-18 17:13:56 +0200

    finish basic functionalities: Code generation, Download of Codes

--- src/js/createCodes.js
+++ src/js/createCodes.js
@@ -3,25 +3,73 @@ import { Modal } from 'bootstrap';
 
 /* Intercept CodeGen Form Submit */
 const codeGenForm = document.getElementById('codeGeneratorForm');
-if( codeGenForm ) {
+if (codeGenForm) {
   codeGenForm.addEventListener('submit', e => {
     e.preventDefault();
     const codeGenForm = e.target;
     const formData = new FormData(codeGenForm).entries();
     const formDataObj = Object.fromEntries(formData);
-    ModalWindow( formDataObj );
+    ModalWindow(formDataObj);
   });
 }
 
-/* Modal Window öffnen */
-const ModalWindow = ( formDataObj ) => {
+/* open Modal Window */
+const ModalWindow = async (formDataObj) => {
   const progressModal = new Modal(document.getElementById('progressModal'), {
     keyboard: false,
     backdrop: 'static'
   });
-  /* Set correct number of total codes in progressModal */
-  document.getElementById('totalCodes').textContent = formDataObj.codeQuantity;
+  /* Prepare ProgressBar */
+  updateModalProgress(0, formDataObj.codeQuantity);
   /* Show Modal Window */
-  progressModal.show();
-  console.log('Form Data Object:', formDataObj);
+  await new Promise(resolve => {
+    progressModal._element.addEventListener('shown.bs.modal', resolve);
+    progressModal.show();
+  });
+  /* Create Codes */
+  const codes = await createUniqeCodes(formDataObj);
+  /* Create Textfile */
+  const textContent = Array.from(codes).join('\n');
+  const textfileURL = createTextFile(textContent);
+  const downloadButton = document.getElementById('codeDownload');
+  downloadButton.href = textfileURL;
+  downloadButton.download = 'codes.txt';
+  downloadButton.classList.remove('d-none');
+};
+
+const createUniqeCodes = async (formDataObj) => {
+  const codes = new Set();
+  while(codes.size < formDataObj.codeQuantity) {
+    const code = createCode(formDataObj.codeLength, formDataObj.codePattern);
+    codes.add(code);
+    await new Promise(resolve => setTimeout(resolve, 0));
+    updateModalProgress(codes.size, formDataObj.codeQuantity);
+  };
+  return codes;
+};
+
+const createCode = (length, patter) => {
+  const charactersLength = patter.length;
+  let code = '';
+  for (let i = 0; i < length; i++) {
+    code += patter.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return code;
+};
+
+const updateModalProgress = (currentCodes, totalCodes) => {
+  const progress = Math.floor((currentCodes / totalCodes) * 100);
+  const progressBar = document.getElementById('dynamicProgressBar');
+  document.getElementById('totalCodes').textContent = totalCodes;
+  document.getElementById('currentCodes').textContent = currentCodes;
+  progressBar.style.width = `${progress}%`;
+  progressBar.setAttribute('aria-valuenow', progress);
+  progressBar.textContent = `${progress}%`;
+};
+
+/* create textfile */
+const createTextFile = (textContent) => {
+  const textFile = new Blob([textContent], { type: 'text/plain' });
+  const textFileURL = URL.createObjectURL(textFile);
+  return textFileURL;
 };
